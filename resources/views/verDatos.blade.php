e5541002d90b5a2d89ad888e98837a2466ed7bb2
Cipher66 <schavezfe16dw@ikzubirimanteo.com>
2017-10-14 23:52:35 +0200

    >El menú es de color amarillo. >El archivo "verDatos.blade.php" ahora tiene menú BS.

--- resources/views/verDatos.blade.php
+++ resources/views/verDatos.blade.php
@@ -1,10 +1,16 @@
-<!DOCTYPE html>
-<html lang="en">
+<!doctype html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang=""> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8" lang=""> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9" lang=""> <![endif]-->
+<!--[if gt IE 8]><!-->
+<html class="no-js" lang="">
+<!--<![endif]-->
+
 <head>
+    <meta charset="utf-8">
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta charset="UTF-8">
-    <title>Registro Completado</title>
+    <title>Diagnostico de vehículos</title>
     <link rel="stylesheet" href="css/bootstrap.min.css">
     <link rel="stylesheet" href="css/jquery.fancybox.css">
     <link rel="stylesheet" href="css/main.css">
@@ -12,6 +18,7 @@
     <link rel="stylesheet" href="css/animate.min.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
 </head>
+
 <body>
 <!-- header section -->
 <section class="banner" role="banner">
@@ -21,7 +28,7 @@
             <nav class="navigation" role="navigation">
                 <ul class="primary-nav">
                     <li><a href="registrarse">Registrarse</a></li>
-                    <li><a href="/#quienes_somos">Quienes somos</a></li>
+                    <li><a href="#quienes_somos">Quienes somos</a></li>
                     <li><a href="http://www.zubirimanteo.hezkuntza.net/web/guest/inicio">Web de Zubiri Manteo</a></li>
                 </ul>
             </nav>
@@ -62,5 +69,15 @@
     </a>
 </footer>
 <!-- footer -->
+
+<!-- JS FILES -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script src="js/jquery.fancybox.pack.js"></script>
+<script src="js/jquery.waypoints.min.js"></script>
+<script src="js/retina.min.js"></script>
+<script src="js/modernizr.js"></script>
+<script src="js/jquery.contact.js"></script>
+<script src="js/main.js"></script>
 </body>
 </html>
